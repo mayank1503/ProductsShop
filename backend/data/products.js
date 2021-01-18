96eb96414cf324a04647d5c5c318edfb20d2c747
mayank1503 <mayankmotwani1503@gmail.com>
2021-01-18 16:03:27 +0530

    Ready for deploy

---
+++ backend/data/products.js
@@ -0,0 +1,75 @@
+const products = [
+  {
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: 'https://lh3.googleusercontent.com/proxy/haFGDg0kI3N8IUYlgg1PtDE-CwYubAW5Jhe46iwsupZ4VNC7aUupiJ4mmsOpNYmSNWQ5f4-1E8N_woQokVSTgDFEySnxkdQ',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio',
+    brand: 'Boat',
+    category: 'Electronics',
+    price: 5,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Sofa Set',
+    image: 'https://ii1.pepperfry.com/media/catalog/product/m/a/800x400/max-three-seater-sofa-in-tan-leatherette-by-furniture-mind-max-three-seater-sofa-in-tan-leatherette--kyb8ol.jpg',
+    description:
+      'Sofa Set for Comfort',
+    brand: 'IKEA',
+    category: 'Home appliances',
+    price: 15,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Cosmetic Products',
+    image: 'https://www.dermstore.com/blog/wp-content/uploads/2018/01/080819-Body-Prods.jpg',
+    description: 'Characterized by varieties a pack of cosmetic products',
+    brand: 'Beardo',
+    category: 'Cosmetics',
+    price: 25,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'LG_Brand',
+    image: 'https://www.lg.com/in/images/TV/features/signature-brand-productslink-d-type2.jpg',
+    description:
+      'The ultimate home entertainment center starts with LEDs. Whether you are into gaming, HD movies, television, music',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 10,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Ponds sandal Tal',
+    image: 'https://cdn2.stylecraze.com/wp-content/uploads/2013/03/Best-Pond%E2%80%99s-Products-Available-In-India-%E2%80%93-Our-Top-10-1.jpg.webp',
+    description:
+      'Radiance talc Natural Sunscren',
+    brand: 'Ponds',
+    category: 'Cosmetics',
+    price: 8,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Detergent Liquid',
+    image: 'https://methodhome.com/wp-content/uploads/laundry_p-v2-500x500.png',
+    description:
+      'Detergent Liquid for extra care',
+    brand: 'Method Home',
+    category: 'Cosmetics',
+    price: 6,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default products
